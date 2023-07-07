8c6e77e57da74c4d94c2d5735f32237077a8216b
Nicolas Merz <nicolasmerz19@gmail.com>
2023-07-06 22:41:37 -0400

    Add black profile for isort

--- lua/plugins/null_ls.lua
+++ lua/plugins/null_ls.lua
@@ -8,7 +8,7 @@ return {
       root_dir = require("null-ls.utils").root_pattern(".null-ls-root", ".neoconf.json", "Makefile", ".git"),
       sources = {
         nls.builtins.formatting.black.with({ extra_args = { "--fast" } }),
-        nls.builtins.formatting.isort,
+        nls.builtins.formatting.isort.with({ extra_args = { "--profile=black" } }),
         -- Ignore warnings already handled by pyright (F401, F821)
         -- E501 is line too long which is annoyingly common
         -- W503 is "Line break occurred before a binary operator" which seems to conflict with a formatter
